2fa744bf05d8e7c32ae97cdcd59f4b6f03b78508
Kaique Moura <kaique29082009@gmail.com>
2026-06-04 01:45:11 -0300

    Update globalsVariables.js
    só adicionei o localStorage pra ficar dinâmico

--- js/globalsVariables.js
+++ js/globalsVariables.js
@@ -1,3 +1,11 @@
-export let sizeCamp = 0;
-export let life = 0;
-export let elementsCamp = [0,0,0,0,0] //barco1, barco2, barco3, bomba, agua
+export let sizeCamp = Number(localStorage.getItem("sizeCamp")) || 4;
+export let life = Number(localStorage.getItem("life")) || 3;
+export let elementsCamp = JSON.parse(localStorage.getItem("elementsCamp")) || [4,4,3,3,2] //barco1, barco2, barco3, bomba, agua
+
+export function configurate(newElwments, newSize, newLife) {
+    elementsCamp.length = 0;
+    elementsCamp.push(newElwments);
+
+    sizeCamp = newSize;
+    life = newLife;
+}
