654b9cb5f2bc849ee93898fb0a8fbd56b8aaa1fd
Kaique Moura <kaique29082009@gmail.com>
2026-06-04 01:44:49 -0300

    Update dificults.js
    role da bixiga, mais até que simples. Adicionei o LocalStorage que o professor comentou, é tipo um banco de dados local, muito dahora, mas chatinho de mexer

--- js/dificults.js
+++ js/dificults.js
@@ -1,20 +1,32 @@
 //#region exportações
-import { life } from "./globalsVariables.js";
-import { elementsCamp } from "./globalsVariables.js";
-import { sizeCamp } from "./globalsVariables.js";
+import { configurate, life, elementsCamp, sizeCamp } from "./globalsVariables.js";
 //#endregion
 
 //region Captura dificuldade e definir dados
 let btnInit = document.querySelector("#submitDificult");
 
-btnInit.addEventListener('click', () => {
-    let option = document.querySelector("#c-dificult")
-    let val = option.value
+if (btnInit) {
+    btnInit.addEventListener('click', () => {
+        let option = document.querySelector("#c-dificult")
+        let val = option.value
 
-    switch (val) {
-        case "1":
-            
-            break;
-    }
-});
+        switch (val) {
+            case "1":
+                localStorage.setItem("sizeCamp", "4");
+                localStorage.setItem("life", "3")
+                localStorage.setItem("elementsCamp", "[4, 4, 3, 3, 2]");//setando os valores de elementCamp, sizeCamp, Life
+                break;
+            case "2":
+                localStorage.setItem("sizeCamp", "8");
+                localStorage.setItem("life", "3")
+                localStorage.setItem("elementsCamp", "[12, 12, 12, 12, 16]");//setando os valores de elementCamp, sizeCamp, Life
+                break;
+            case "3":
+                localStorage.setItem("sizeCamp", "12");
+                localStorage.setItem("life", "3")
+                localStorage.setItem("elementsCamp", "[28, 29, 29, 29, 29]");//setando os valores de elementCamp, sizeCamp, Life
+                break;
+        }
+    });
+}
 //#endregion
