5f99fff3ab08cad33b77c3cfc167f8d09efc54f2
Kaique Moura <kaique29082009@gmail.com>
2026-06-04 00:51:31 -0300

    Create dificults.js

---
+++ js/dificults.js
@@ -0,0 +1,20 @@
+//#region exportações
+import { life } from "./globalsVariables.js";
+import { elementsCamp } from "./globalsVariables.js";
+import { sizeCamp } from "./globalsVariables.js";
+//#endregion
+
+//region Captura dificuldade e definir dados
+let btnInit = document.querySelector("#submitDificult");
+
+btnInit.addEventListener('click', () => {
+    let option = document.querySelector("#c-dificult")
+    let val = option.value
+
+    switch (val) {
+        case "1":
+            
+            break;
+    }
+});
+//#endregion
