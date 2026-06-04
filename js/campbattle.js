2cd959de48840d4b8dd180624deb742dae95b933
Kaique Moura <kaique29082009@gmail.com>
2026-06-04 00:51:03 -0300

    Update campbattle.js
    aqui tem tanta coisa que nem compensa escrever aqui, mas resumindo:
    
    Cria uma matriz aleatório de acordo com a quantidade de elementos => elementCamp [1,1,1,1,1] - barco1, barco2, barco3, explosão, água, nesse caso então 5 (numeros aleatórios serão de 0-4 então)
    
    ele sorteia um indice aleatório do 'elementCamp', verifica se >0, se for ele adiciona o numero sorteado e vai pra próxima coluna, se não ele tenta outro número
    
    depois de criada a matriz. ele troca as imagens de acordo com os números:
    
    0, 1, 2, 3, 4 => barc1, barc2, barc3, explosão, água

--- js/campbattle.js
+++ js/campbattle.js
@@ -1,32 +1,51 @@
 //#region Importações
-import { sizeCamp } from './dificult.js';
+import { sizeCamp } from './globalsVariables.js';
+import { elementsCamp } from './globalsVariables.js';
 //#endregion
 
+//#region Gerando o campo
 console.log("campBattle.js está ativo!"); //Só pra verficar se fuinciona
 
 let camp = document.querySelector(".campBattle"); // captura o campBattle do index.html
 camp.style.gridTemplateRows = `repeat(${sizeCamp}, 1fr)`;
 camp.style.gridTemplateColumns = `repeat(${sizeCamp}, 1fr)`;
 
+let imgsInit = ['url(imgs/wood2.png)', 'url(imgs/wood1.png', 'url(imgs/wood3.png)'];
+
 for (let i = 0; i < sizeCamp; i++) {
     for (let j = 0; j < sizeCamp; j++) {
+        let alet = Math.floor(Math.random() * 3);
         let cel = document.createElement('div');
         cel.classList.add('celCamp');
-        cel.style.backgroundImage = 'url("imgs/imgTest.png")';
+        cel.style.backgroundImage = imgsInit[alet];
 
         camp.append(cel);
     }
 }
+//#endregion
 
 //#region criando Matriz
-let itensCamp = []
+function checkNumsElementsCamp(elementsArray) {
+    return elementsArray.some(fld => fld > 0)
+    console.log(checkNumsElementsCamp(elementsArray))
+}
 
-for (let i = 0; i < sizeCamp; i++) {
-    let lin = [];
-    for (let j = 0; j < sizeCamp; j++) {
-        lin.push(0);
+let itensCamp = []; //local que terá todos os indices
+
+for (let i = 0; i < sizeCamp; i++) { //manupula as linhas da matriz
+    let lin = []; //guardar os elementos de cada linha
+    let j = 0;
+
+    while(j < sizeCamp){
+        let randNum = Math.floor(Math.random() * elementsCamp.length); //cria um número aleatório dependendo da quantidade de elementos nesse caso 0 a 4
+
+        if(elementsCamp[randNum] > 0){//se o indece sorteado ainda tiver 'elementos' adiciona ele e move pra proxima coluna
+            lin.push(randNum);
+            elementsCamp[randNum]--;
+            j++;
+        }
     }
-    itensCamp.push(lin);
+    itensCamp.push(lin);//adiciona a linha completa na matriz
 }
 
 console.log(itensCamp)
@@ -34,18 +53,23 @@ console.log(itensCamp)
 
 //#region evento de clicar
 let cCel = document.querySelectorAll('.celCamp');
+let imgsShip = ['ship1.png', 'ship2.png', 'ship3.png', 'explode.png', 'sea.png'];
 
 itensCamp.forEach((row, rowIndex) => {
     row.forEach((Element, colIndex) => {
         let cel = cCel[rowIndex * sizeCamp + colIndex];
         cel.addEventListener('click', () => {
-            cel.style.transform = "rotateY(-180deg)";
+            cel.style.transform = "rotateY(-360deg) rotateX(-35deg)";
 
             setTimeout(() => {
-                switch(Element){
-                    case 0: cel.style.backgroundImage = ""; break;
+                switch (Element) {
+                    case 0: cel.style.backgroundImage = `url(imgs/${imgsShip[4]})`; cel.style.border = "none"; break;
+                    case 1: cel.style.backgroundImage = `url(imgs/${imgsShip[3]})`; cel.style.border = "none"; break;
+                    case 2: cel.style.backgroundImage = `url(imgs/${imgsShip[2]})`; cel.style.border = "none"; break;
+                    case 3: cel.style.backgroundImage = `url(imgs/${imgsShip[1]})`; cel.style.border = "none"; break;
+                    case 4: cel.style.backgroundImage = `url(imgs/${imgsShip[0]})`; cel.style.border = "none"; break;
                 }
-            }, 100);
+            }, 200);
         });
     });
 });
