d97b33f99ac02cf3db1b69bd92da77a3fffc6eab
Kaique Moura <kaique29082009@gmail.com>
2026-06-04 21:20:11 -0300

    Update globalsVariables.js

--- js/globalsVariables.js
+++ js/globalsVariables.js
@@ -2,10 +2,14 @@ export let sizeCamp = Number(localStorage.getItem("sizeCamp")) || 4;
 export let life = Number(localStorage.getItem("life")) || 3;
 export let elementsCamp = JSON.parse(localStorage.getItem("elementsCamp")) || [4,4,3,3,2] //barco1, barco2, barco3, bomba, agua
 
-export function configurate(newElwments, newSize, newLife) {
+export function configurate(newElements, newSize, newLife) {
     elementsCamp.length = 0;
-    elementsCamp.push(newElwments);
+    elementsCamp.push(...newElements);
 
     sizeCamp = newSize;
     life = newLife;
+
+    localStorage.setItem("elementsCamp", JSON.stringify(elementsCamp));
+    localStorage.setItem("sizeCamp", sizeCamp);
+    localStorage.setItem("life", life);
 }
