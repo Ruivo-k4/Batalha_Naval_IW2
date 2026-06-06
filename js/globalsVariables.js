05afcab914120697d15e5d99031fc178841fa272
Kaique Moura <kaique29082009@gmail.com>
2026-06-06 15:27:01 -0300

    Update globalsVariables.js

--- js/globalsVariables.js
+++ js/globalsVariables.js
@@ -1,3 +1,5 @@
+console.log("global,Variables.js está ativo!"); //Só pra verficar se fuinciona
+
 export let sizeCamp = Number(localStorage.getItem("sizeCamp")) || 4;
 export let life = Number(localStorage.getItem("life")) || 3;
 export let elementsCamp = JSON.parse(localStorage.getItem("elementsCamp")) || [4,4,3,3,2] //barco1, barco2, barco3, bomba, agua
