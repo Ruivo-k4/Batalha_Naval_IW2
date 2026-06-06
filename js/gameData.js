7329fefba88a23000d2fb2b9222638cc9f93467c
Kaique Moura <kaique29082009@gmail.com>
2026-06-06 15:26:59 -0300

    Update gameData.js

--- js/gameData.js
+++ js/gameData.js
@@ -1,3 +1,5 @@
+console.log("gameData.js está ativo!"); //Só pra verficar se fuinciona
+
 //#region Importações
 import { life, elementsCamp, configurate } from "./globalsVariables.js";
 //#endregion
@@ -9,16 +11,21 @@ if (cLifes) {
     for (let i = 0; i < life; i++) {
         let heart = document.createElement('i');
         heart.className = 'bi bi-heart-fill';
+        heart.id = "iconHeart"
 
         cLifes.append(heart);
     }
 }
+
+let cHearts = document.querySelectorAll("#iconHeart");
+
+cHearts.forEach(element => console.log(element));
 //#endregion
 
 //#region Barcos
 let totShips = 0;
 
 for(let i = 0; i < 3; i++) {
-    alert(elementsCamp[i])
+    
 }
 //#endregion
