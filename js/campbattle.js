6e83a1c3cbd806b87d3df80328b62ead1152dd4a
Kaique Moura <kaique29082009@gmail.com>
2026-06-07 01:28:54 -0300

    Update campbattle.js
    adicionei um sistema de vidas baseado em icones

--- js/campbattle.js
+++ js/campbattle.js
@@ -1,7 +1,7 @@
 console.log("campBattle.js está ativo!"); //Só pra verficar se fuinciona
 
 //#region Importações
-import { sizeCamp, elementsCamp } from './globalsVariables.js';
+import { sizeCamp, elementsCamp, life, configurate } from './globalsVariables.js';
 //#endregion
 
 //#region Gerando o campo
@@ -30,10 +30,10 @@ for (let i = 0; i < sizeCamp; i++) { //manupula as linhas da matriz
     let lin = []; //guardar os elementos de cada linha
     let j = 0;
 
-    while(j < sizeCamp){
+    while (j < sizeCamp) {
         let randNum = Math.floor(Math.random() * elementsCamp.length); //cria um número aleatório dependendo da quantidade de elementos nesse caso 0 a 4
 
-        if(elementsCamp[randNum] > 0){//se o indece sorteado ainda tiver 'elementos' adiciona ele e move pra proxima coluna
+        if (elementsCamp[randNum] > 0) {//se o indece sorteado ainda tiver 'elementos' adiciona ele e move pra proxima coluna
             lin.push(randNum);
             elementsCamp[randNum]--;
             j++;
@@ -57,15 +57,67 @@ itensCamp.forEach((row, rowIndex) => {
 
             setTimeout(() => {
                 switch (Element) {
-                    case 0: cel.style.backgroundImage = `url(imgs/${imgsShip[0]})`; cel.style.border = "none"; break;
-                    case 1: cel.style.backgroundImage = `url(imgs/${imgsShip[1]})`; cel.style.border = "none"; break;
-                    case 2: cel.style.backgroundImage = `url(imgs/${imgsShip[2]})`; cel.style.border = "none"; break;
-                    case 3: cel.style.backgroundImage = `url(imgs/${imgsShip[3]})`; cel.style.border = "none"; break;
-                    case 4: cel.style.backgroundImage = `url(imgs/${imgsShip[4]})`; cel.style.border = "none"; break;
+                    case 0: cel.style.backgroundImage = `url(imgs/${imgsShip[0]})`; break;
+                    case 1: cel.style.backgroundImage = `url(imgs/${imgsShip[1]})`; break;
+                    case 2: cel.style.backgroundImage = `url(imgs/${imgsShip[2]})`; break;
+                    case 3: cel.style.backgroundImage = `url(imgs/${imgsShip[3]})`; stateHearts(); break;
+                    case 4: cel.style.backgroundImage = `url(imgs/${imgsShip[4]})`; break;
                 }
             }, 200);
         });
     });
 });
+
+//region Vidas do jogo
+
+//#region criando corações
+let cLifes = document.querySelector(".heartsLife")//captura a div dos corações
+let heartList = []
+
+if (cLifes) {
+    for (let i = 0; i < life; i++) {
+        let heart = document.createElement('i');
+        heart.className = 'iconHeart bi bi-heart-fill';
+
+        cLifes.append(heart);
+        heartList.push(3);
+    }
+
+    console.log("Lista: " + heartList)
+}
+//#endregion
+
+//#region perdendo vida
+let indiceHeart = 0;
+function stateHearts() {
+    if (indiceHeart < heartList.length) {
+        heartList[indiceHeart]--;
+
+        if (heartList[indiceHeart] <= 0) {
+            indiceHeart++;
+        }
+
+        StateFrame();
+    }
+    console.log(heartList)
+}
+function StateFrame() {
+    let cIconsHeart = document.querySelectorAll(".iconHeart")
+
+    cIconsHeart.forEach((element, index) => {
+        if (heartList[index] === 2) {
+            element.className = 'iconHeart bi bi-heart-half';
+        } else if (heartList[index] === 1) {
+            element.className = 'iconHeart bi bi bi-heart';
+        } else if (heartList[index] <= 0) {
+            element.className = 'iconHeart bi bi-heartbreak';
+            
+            setTimeout( ()=> {
+                alert("PERDEU BOBÃO")
+            }, 200)
+        }
+    });
+}
+//#endregion
 
 //#endregion
