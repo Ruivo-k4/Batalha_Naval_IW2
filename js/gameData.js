7f70a1149d9ac15d6efaba1ab72a0a9a82166475
Kaique Moura <kaique29082009@gmail.com>
2026-06-07 01:30:16 -0300

    Update gameData.js
    o sistemas de vida vai ser feito completamente pelo 'campBattle.js'

--- js/gameData.js
+++ js/gameData.js
@@ -1,31 +1,14 @@
 console.log("gameData.js está ativo!"); //Só pra verficar se fuinciona
 
 //#region Importações
-import { life, elementsCamp, configurate } from "./globalsVariables.js";
+import { life, elementsCamp, configurate} from "./globalsVariables.js";
 //#endregion
 
-//#region Vidas
-let cLifes = document.querySelector(".heartsLife")//captura a div dos corações
-
-if (cLifes) {
-    for (let i = 0; i < life; i++) {
-        let heart = document.createElement('i');
-        heart.className = 'bi bi-heart-fill';
-        heart.id = "iconHeart"
-
-        cLifes.append(heart);
-    }
-}
-
-let cHearts = document.querySelectorAll("#iconHeart");
-
-cHearts.forEach(element => console.log(element));
-//#endregion
 
 //#region Barcos
 let totShips = 0;
 
-for(let i = 0; i < 3; i++) {
-    
+for (let i = 0; i < 3; i++) {
+
 }
 //#endregion
