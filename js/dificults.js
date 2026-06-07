e5ed707e05cb6c41dfc197ceb618b0fca3150cf0
Kaique Moura <kaique29082009@gmail.com>
2026-06-07 01:29:41 -0300

    Update dificults.js
    as dificuldades tem regras agora: o total de bombas obrigatoriamente deve ser um múltiplo de 3

--- js/dificults.js
+++ js/dificults.js
@@ -15,18 +15,18 @@ if (btnInit) {
         switch (val) {
             case "1":
                 localStorage.setItem("sizeCamp", "4");
-                localStorage.setItem("life", "3")
-                localStorage.setItem("elementsCamp", "[4, 4, 3, 3, 2]");//setando os valores de elementCamp, sizeCamp, Life
+                localStorage.setItem("life", "1")
+                localStorage.setItem("elementsCamp", "[3, 3, 2, 3, 5]");//setando os valores de elementCamp, sizeCamp, Life
                 break;
             case "2":
-                localStorage.setItem("sizeCamp", "8");
+                localStorage.setItem("sizeCamp", "4");
                 localStorage.setItem("life", "4")
                 localStorage.setItem("elementsCamp", "[12, 12, 12, 12, 16]");//setando os valores de elementCamp, sizeCamp, Life
                 break;
             case "3":
-                localStorage.setItem("sizeCamp", "12");
+                localStorage.setItem("sizeCamp", "10");
                 localStorage.setItem("life", "9")
-                localStorage.setItem("elementsCamp", "[28, 29, 29, 29, 29]");//setando os valores de elementCamp, sizeCamp, Life
+                localStorage.setItem("elementsCamp", "[28, 29, 29, 30, 28]");//setando os valores de elementCamp, sizeCamp, Life
                 break;
         }
     });
