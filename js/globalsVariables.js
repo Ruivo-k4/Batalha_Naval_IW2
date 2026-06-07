9f05618978f4481d0d75029b21d516dec8c5a8cf
Kaique Moura <kaique29082009@gmail.com>
2026-06-07 01:30:33 -0300

    Update globalsVariables.js
    alterei, alterei, mas não mudei nada

--- js/globalsVariables.js
+++ js/globalsVariables.js
@@ -2,9 +2,9 @@ console.log("global,Variables.js está ativo!"); //Só pra verficar se fuinciona
 
 export let sizeCamp = Number(localStorage.getItem("sizeCamp")) || 4;
 export let life = Number(localStorage.getItem("life")) || 3;
-export let elementsCamp = JSON.parse(localStorage.getItem("elementsCamp")) || [4,4,3,3,2] //barco1, barco2, barco3, bomba, agua
+export let elementsCamp = JSON.parse(localStorage.getItem("elementsCamp")) || [3,3,2,3,5] //barco1, barco2, barco3, bomba, agua
 
-export function configurate(newElements, newSize, newLife) {
+export function configurate(newElements, newSize, newLife, newHeart) {
     elementsCamp.length = 0;
     elementsCamp.push(...newElements);
 
