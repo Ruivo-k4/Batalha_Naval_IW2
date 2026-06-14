eca4182204df0c95e0f6e6cebc90cc9cda3fb1c4
Kaique Moura <kaique29082009@gmail.com>
2026-06-14 04:35:07 -0300

    Update gameData.js

--- js/gameData.js
+++ js/gameData.js
@@ -3,12 +3,3 @@ console.log("gameData.js está ativo!"); //Só pra verficar se fuinciona
 //#region Importações
 import { life, elementsCamp, configurate} from "./globalsVariables.js";
 //#endregion
-
-
-//#region Barcos
-let totShips = 0;
-
-for (let i = 0; i < 3; i++) {
-
-}
-//#endregion
