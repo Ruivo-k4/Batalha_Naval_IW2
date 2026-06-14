2860bed92f6de4686ad8e79a0c3057215a9ead7f
Kaique Moura <kaique29082009@gmail.com>
2026-06-14 04:34:43 -0300

    Update campbattle.js
    "totShip" é uma variavel global que contem a quantidade de barcos disponivel para aquele nivel em específico, ele é setado em 'dificults.js'

--- js/campbattle.js
+++ js/campbattle.js
@@ -1,8 +1,23 @@
 console.log("campBattle.js está ativo!"); //Só pra verficar se fuinciona
 
 //#region Importações
-import { sizeCamp, elementsCamp, life, configurate } from './globalsVariables.js';
+import { sizeCamp, elementsCamp, life, configurate, totShips } from './globalsVariables.js';
 //#endregion
+
+let shipsTot = 0;//dita quando o jogo acaba
+console.log(totShips);
+
+function winner() {
+    shipsTot += 1;
+    console.log(totShips)
+
+    if (shipsTot == totShips) {
+        setTimeout(() => {
+            alert("Você venceu!!");
+            location.reload();
+        }, 1000);
+    }
+}
 
 //#region Gerando o campo
 let camp = document.querySelector(".campBattle"); // captura o campBattle do index.html
@@ -45,6 +60,61 @@ for (let i = 0; i < sizeCamp; i++) { //manupula as linhas da matriz
 console.log(itensCamp)
 //#endregion
 
+//#region combos
+let score = 0
+let combo = 0
+let comboImg = document.querySelector("#comboImage")
+
+function comboSequence() {
+    let scoreDisplay = document.querySelector("#scoreText");
+    combo += 1;
+
+    //ajuste dos multiplicadores de comapo (CM)
+    if (combo >= 0 && combo < totShips * 0.25) {
+        score += 100;
+    } else if (combo >= totShips * 0.25 && combo < totShips * 0.37) {
+        score += 100 * 2;
+    } else if (combo >= totShips * 0.37 && combo < totShips * 0.5) {
+        score += 100 * 4;
+    } else if (combo >= totShips * 0.5 && combo < totShips * 0.90) {
+        score += 100 * 8;
+    } else {
+        score += 100 * 10;
+    }
+
+    console.log(combo)
+
+    if (comboImg) {
+        comboImg.style.display = "flex";
+        comboImg.style.opacity = "1";
+
+        //sistema de troca de imagens dinâmico
+        if (combo == Math.round(totShips * 0.25)) {
+            comboImg.src = "imgs/combo.png";
+        } else if (combo == Math.round(totShips * 0.37)) {
+            comboImg.src = "imgs/superCombo.png";
+        } else if (combo == Math.round(totShips * 0.5)) {
+            comboImg.src = "imgs/hiperCombo.png";
+        } else if (combo == Math.round(totShips * 0.90)) {
+            comboImg.src = "imgs/comboSea.png";
+        }
+        else {
+            comboImg.style.display = "none"
+        }
+
+        setTimeout(() => { // dá um tempo antes de sumir
+            comboImg.style.opacity = "0";
+            setTimeout(() => {
+                comboImg.style.display = "none";
+            }, 1000);
+        }, 1500);
+    }
+
+    scoreDisplay.innerHTML = "Pontos: " + score;
+    return (score, combo)
+}
+//#endregion
+
 //#region evento de clicar
 let cCel = document.querySelectorAll('.celCamp');
 let imgsShip = ['ship1.png', 'ship2.png', 'ship3.png', 'explode.png', 'sea.png'];
@@ -57,18 +127,19 @@ itensCamp.forEach((row, rowIndex) => {
 
             setTimeout(() => {
                 switch (Element) {
-                    case 0: cel.style.backgroundImage = `url(imgs/${imgsShip[0]})`; break;
-                    case 1: cel.style.backgroundImage = `url(imgs/${imgsShip[1]})`; break;
-                    case 2: cel.style.backgroundImage = `url(imgs/${imgsShip[2]})`; break;
-                    case 3: cel.style.backgroundImage = `url(imgs/${imgsShip[3]})`; stateHearts(); break;
-                    case 4: cel.style.backgroundImage = `url(imgs/${imgsShip[4]})`; break;
+                    case 0: cel.style.backgroundImage = `url(imgs/${imgsShip[0]})`; winner(); comboSequence(); break;
+                    case 1: cel.style.backgroundImage = `url(imgs/${imgsShip[1]})`; winner(); comboSequence(); break;
+                    case 2: cel.style.backgroundImage = `url(imgs/${imgsShip[2]})`; winner(); comboSequence(); break;
+                    case 3: cel.style.backgroundImage = `url(imgs/${imgsShip[3]})`; stateHearts(); combo = 0; break;
+                    case 4: cel.style.backgroundImage = `url(imgs/${imgsShip[4]})`; combo = 0; break;
                 }
             }, 200);
         });
     });
 });
+//#endregion
 
-//region Vidas do jogo
+//#region Vidas do jogo
 
 //#region criando corações
 let cLifes = document.querySelector(".heartsLife")//captura a div dos corações
@@ -101,6 +172,7 @@ function stateHearts() {
     }
     console.log(heartList)
 }
+
 function StateFrame() {
     let cIconsHeart = document.querySelectorAll(".iconHeart")
 
@@ -111,13 +183,33 @@ function StateFrame() {
             element.className = 'iconHeart bi bi bi-heart';
         } else if (heartList[index] <= 0) {
             element.className = 'iconHeart bi bi-heartbreak';
-            
-            setTimeout( ()=> {
-                alert("PERDEU BOBÃO")
+
+            setTimeout(() => {
+                alert("PERDEU BOBÃO");
+                location.reload();//recarrega a página
             }, 200)
         }
     });
 }
 //#endregion
 
+//#endregion
+
+//#region "responsividade" campo
+if (life == 1) {
+    cCel.forEach(element => {
+        element.style.height = "60px";
+        element.style.width = "60px"
+    });
+} else if (life == 4) {
+    cCel.forEach(element => {
+        element.style.height = "50px";
+        element.style.width = "50px"
+    });
+} else {
+    cCel.forEach(element => {
+        element.style.height = "40px";
+        element.style.width = "40px"
+    });
+}
 //#endregion
