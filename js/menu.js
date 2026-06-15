b14e09056e76571be1ac6e396829f057365cd6ef
Kaique Moura <kaique29082009@gmail.com>
2026-06-15 14:18:55 -0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -24,18 +24,18 @@ if (restartImg) {
 
         if (clicks === 1) {
             localStorage.setItem("sizeCamp", "4");
-            localStorage.setItem("life", "1")
+            localStorage.setItem("life", "1");
             localStorage.setItem("elementsCamp", "[3, 3, 2, 3, 5]");
             location.reload();
         } else if (clicks === 2) {
             localStorage.setItem("sizeCamp", "6");
-            localStorage.setItem("life", "2")
+            localStorage.setItem("life", "2");
             localStorage.setItem("elementsCamp", "[7, 7, 8, 7, 9]");
             location.reload();
         } else if (clicks === 0) {
             localStorage.setItem("sizeCamp", "10");
-            localStorage.setItem("life", "9")
-            localStorage.setItem("elementsCamp", "[17, 14, 15, 27, 27]");
+            localStorage.setItem("life", "6");
+            localStorage.setItem("elementsCamp", "[15, 17, 19, 21, 29]");
             location.reload();
         }
     });
