bd52f9da7049978d1991bc7cedb5ad70196f594d
Kaique Moura <kaique29082009@gmail.com>
2026-06-15 14:18:50 -0300

    Update dificults.js

--- js/dificults.js
+++ js/dificults.js
@@ -15,18 +15,18 @@ if (btnInit) { //esse daqui é da página de inicio
         switch (val) {
             case "1":
                 localStorage.setItem("sizeCamp", "4");
-                localStorage.setItem("life", "1")
+                localStorage.setItem("life", "1");
                 localStorage.setItem("elementsCamp", "[3, 3, 2, 3, 5]");//setando os valores de elementCamp, sizeCamp, Life
                 break;
             case "2":
                 localStorage.setItem("sizeCamp", "6");
-                localStorage.setItem("life", "2")
+                localStorage.setItem("life", "2");
                 localStorage.setItem("elementsCamp", "[7, 7, 8, 7, 9]");
                 break;
             case "3":
                 localStorage.setItem("sizeCamp", "10");
-                localStorage.setItem("life", "9")
-                localStorage.setItem("elementsCamp", "[17, 14, 15, 27, 27]");
+                localStorage.setItem("life", "6");
+                localStorage.setItem("elementsCamp", "[15, 17, 19, 21, 29]");
                 break;
         }
     });
