8b34c2ad8bb7e5cb64c92f0a3b9b2153de5e0959
Kaique Moura <kaique29082009@gmail.com>
2026-06-17 00:16:51 -0300

    Update gameData.js

--- js/gameData.js
+++ js/gameData.js
@@ -1,5 +1,5 @@
 console.log("gameData.js está ativo!"); //Só pra verficar se fuinciona
 
 //#region Importações
-import { life, elementsCamp, configurate} from "./globalsVariables.js";
+import { life, elementsCamp, configurate, nameUser } from "./globalsVariables.js";
 //#endregion
