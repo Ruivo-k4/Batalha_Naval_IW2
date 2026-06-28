2d4c6791e39d0a54a48f80850df2267ee79b165d
Kaique Moura <kaique29082009@gmail.com>
2026-06-28 01:12:53 -0300

    Update scoreDisplay.js

--- js/scoreDisplay.js
+++ js/scoreDisplay.js
@@ -1,26 +1,44 @@
 console.log("scoreDisplay.js está funcionando!!");
 
 //#region importações
-import { nameUser } from "./globalsVariables.js";
+import { nameUser, scoreUser, positionUser, game } from "./globalsVariables.js";
 //#endregion
 
+//#region tabela
 let table = document.querySelector("#content_data");
 
 if (table) {
     let body = document.querySelector("#table_body");
-    let data = [nameUser, nameUser, 10]
-    
-    for (let i = 0; i < 10; i++) {
-        let row = document.createElement('tr');
-        row.classList.add = "table_row";
+    let data = [nameUser, scoreUser, positionUser];
+    let pos = positionUser;
 
-        for(let j = 0; j < 3; j++){
-            let col = document.createElement('th');
-            col.classList.add = "table_cow";
+    let row = document.createElement('tr');
+    row.classList.add = "table_row";
 
-            col.innerHTML = data[j];
-            row.append(col);
-        }
-        body.append(row);
+    for (let j = 0; j < 3; j++) {
+        let col = document.createElement('th');
+        col.classList.add = "table_cow";
+
+        col.innerHTML = data[j];
+        row.append(col);
     }
+    body.append(row);
+
+    if (game === "play") {
+        localStorage.setItem("gameState", "end");
+        pos += 1;
+    }
+
+    localStorage.setItem("positionUser", pos);
 }
+//#endregion
+
+//#region resetar
+let reset = document.querySelector("#reset_footer");
+
+if (reset) {
+    reset.addEventListener('click', () => {
+
+    })
+}
+//#endregion
